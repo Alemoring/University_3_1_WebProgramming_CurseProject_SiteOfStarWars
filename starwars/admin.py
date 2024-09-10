598be69079d65e2c1a2dc481a81500627c37c979
Alemoring <morgunov2004@list.ru>
2024-09-10 13:15:09 +0800

    add new model

--- starwars/admin.py
+++ starwars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from starwars.models import Jedi
+
 # Register your models here.
+@admin.register(Jedi)
+class JediAdmin(admin.ModelAdmin):
+    list_display = ['name', 'padavan']
--- starwars/models.py
+++ starwars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Race(models.Model):
+    name = models.TextField("Название")
+
+class Jedi(models.Model):
+    name = models.TextField("Имя")
+    padavan = models.TextField("Падаван")
+    race = models.ForeignKey("Race", on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "Джедай"
+        verbose_name_plural = "Джедаи"
