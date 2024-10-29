6c94408d14f57bd4ad3d0e11abf0b4fb207951b9
Alemoring <morgunov2004@list.ru>
2024-10-29 13:15:02 +0800

    start work for Vue

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import axios from "axios"
+
+const list  = ref([4,3,2,5,1,2])
+
+async function onLoadClick() {
+  axios.get("/api/characters/")
+}
+</script>
+
+<template>
+  <div>
+    <div v-for="item in list">
+      <div>{{ item }}</div>
+    </div>
+
+    <button @click="onLoadClick()">Загрузить</button>
+  </div>
+
+</template>
+
+<style scoped></style>
