a7497e8384d41d71dc01f676cb15e00c984f3a64
Alemoring <morgunov2004@list.ru>
2024-11-12 12:50:59 +0800

    CRUD operations on frontend characters

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,7 @@ const characters = ref([])
 const races = ref([])
 const fractions = ref([])
 const characterToAdd = ref({})
+const characterToEdit = ref({})
 
 async function fetchCharacters() {
   const r1 = await axios.get("/api/characters/")
@@ -18,6 +19,17 @@ async function fetchCharacters() {
   const r3 = await axios.get("/api/fractions/")
   console.log(r3.data)
   fractions.value = r3.data
+}
+
+async function onCharacterEditClick(character) {
+  characterToEdit.value = { ...character };
+}
+
+async function onUpdateCharacter() {
+  await axios.put(`/api/characters/${characterToEdit.value.id}/`, {
+    ...characterToEdit.value,
+  });
+  await fetchCharacters();
 }
 
 async function onCharacterAdd(){
@@ -44,14 +56,61 @@ onBeforeMount(async () => {
 </script>
 
 <template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+      <a class="navbar-brand" href="#">Главная</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Персонажи</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Рассы</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Фракции</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Планеты</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Корабли</a>
+          </li>
+        </ul>
+      </div>
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Пользователь</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/admin">Админка</a></li>
+            </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>  
+
   <div>
     <div v-for="item in characters" class="character-item">
       <div>{{ item.name }}</div>
+      <div>{{ races[item.race]?.name }}</div>
+      <button
+        class="btn btn-success"
+        @click="onCharacterEditClick(item)"
+        data-bs-toggle="modal"
+        data-bs-target="#editCharacterModal"
+      >
+        <i class="bi bi-pen-fill"></i>
+      </button>
       <button class="btn btn-danger" @click="onRemoveClick(item)">
         <i class="bi bi-x"></i>
       </button>
     </div>
+</div>
 
+  <div>
     <button @click="onLoadClick()">Загрузить</button>
   </div>
 
@@ -92,6 +151,67 @@ onBeforeMount(async () => {
     </div>
   </div>
 </form>
+
+<div class="modal fade" id="editCharacterModal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">
+            редактировать
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col">
+              <div class="form-floating">
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="characterToEdit.name"
+                />
+                <label for="floatingInput">Имя</label>
+              </div>
+            </div>
+            <div class="col-auto">
+              <div class="form-floating">
+                <select class="form-select" v-model="characterToEdit.race">
+                  <option :value="rc.id" v-for="rc in races">
+                    {{ rc.name }}
+                  </option>
+                </select>
+                <label for="floatingInput">Расса</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Закрыть
+          </button>
+          <button
+            data-bs-dismiss="modal"
+            type="button"
+            class="btn btn-primary"
+            @click="onUpdateCharacter"
+          >
+            Сохранить
+          </button>
+        </div>
+      </div>
+    </div>
+</div>
+
+
 
 </template>
 
